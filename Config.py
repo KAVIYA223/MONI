4f71eb6f29acb56c53035c7c9dc10e5ed7a7e5e7
KAVIYA223 <123254596+KAVIYA223@users.noreply.github.com>
2023-01-21 23:29:46 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,18 +1,18 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "17323890"))
-    API_HASH = os.environ.get("API_HASH", "6bd39253d6505762ec92cee9bbeec8bb")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5983809475:AAEkhDHjL2HDnTH0CDycSZiq2jlfpBS7_mY")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "BQAvi3OVLRdWN3mcuhgygP5KdpO_UpCXuYsfoJPZCyuvqvZZtlHYaW0OPWAy5L4ofEMi9aedyzqhtDqMa_fWxeqbLTRWrKHcdhFlrjOHTpMDoVFMsAY3lpj6ikSAkGdj6IqeDdEc7ko_aorT3Pnro5wxXpK8bzqpE2P1IuEacX7xdrNfebZwfLumMuBodtDJREtW_LHEkBw02-WYJXcSIGjv0k8ctiIzw_JvIZOUFI4VTf-jrpJ0dSpOPfrqxXgneMXg7fTBebTbbe6vlbIbKRH4TKudbQCpijWDOGBONZabKzmbSVg_2Uu12Xoi0_6AbZ7ncEbbw8Qs3JyYsKy58owuAAAAAUA84eUA")
-    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
+    API_ID = int(os.environ.get("APP_ID", "13726853"))
+    API_HASH = os.environ.get("API_HASH", "749e522c40616d1fee242fe8974527b9")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5781064175:AAFcZq82PrsrNrGw0zx2M5_wGT8pIMGDgpA")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "BQBKlqRKPwKYiouUV2m1xvt2reRTyjSGFRHLJi7nR5szKlSWXl9_A4T67laLClX8SXn_jBccmV61r-5ZjrWadZ9yFx-N0VZn-XiZ2iY3MtYAxeru8mZECsfiN9c3mfKy6NYXXkrKV53A1yft0P8Y0zrf6jZ-D3V5jPToUtHR4SbMjsLfZNFOiuVinb1qwV30nT89RUvbkE-xm6dHXJQYY863Zce-9P0zxqfFF5VHjS8Kgavh8VFwJ4xWZ9GLf9VMrvZmgjeJijvALZ47SCcEfrnD4I--9TQqtRw72FHZX57WPp1vYyFqpZ3Ycng7TFkxN1zg13F9L9hmCcGNy7QWxfU-AAAAAS8lsr0A")
+    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", "True")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Devil_x_music_bot")
-    SUPPORT = os.environ.get("SUPPORT", "https://t.me/sabyahaapnehai") # Your Support
-    CHANNEL = os.environ.get("CHANNEL", "https://t.me/Teams_devil") # Your Channel
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Dreamxgirl_robot")
+    SUPPORT = os.environ.get("SUPPORT", "https://t.me/mirachlenetwork") # Your Support
+    CHANNEL = os.environ.get("CHANNEL", "https://t.me/darkdevilfederation") # Your Channel
     Source_code = os.environ.get("Source_code"," abe laude  repo chahiye to group join kar 🤨🤨") #your Source_code
-    START_IMG = os.environ.get("START_IMG", "https://te.legra.ph/file/490d4f6fa16e4bdff4c0c.jpg")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5372699109")) # telegram I'd not Username
+    START_IMG = os.environ.get("START_IMG", "https://te.legra.ph/file/44bb9002b357dac5c2d6a.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://te.legra.ph/file/6e5cd3bf33db8bd03a91c.jpg")
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5085967037")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
-    AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
+    AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', "True") # Change it to "True"
